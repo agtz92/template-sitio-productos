82d0d0c7f36bd79a39d0643846bc0a8f984ee9cd
agtz92 <alfredo.gtz92@gmail.com>
2022-03-13 19:14:28 -0600

    Update categories.js

--- src/templates/categories.js
+++ src/templates/categories.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { graphql } from 'gatsby'
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
 import Layout from "../components/layouts/Layout";
 import ProductPreview from "../components/symbols/ProductPreview";
 import Grid3x3 from "../components/wrappers/Grid3x3";
@@ -7,7 +8,12 @@ import Grid3x3 from "../components/wrappers/Grid3x3";
 import "../webflow_styles/normalize.css";
 import "../webflow_styles/webflow.css";
 
-const Categorias = ({data}) => {
+const Categorias = ({ pageContext, data }) => {
+  const { categoria } = pageContext;
+  const { edges, totalCount } = data.allMarkdownRemark;
+  const categoriaHeader = `${totalCount} post${
+    totalCount === 1 ? "" : "s"
+  } in "${categoria}"`;
   const smallDescriptionDummy =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla";
 
@@ -57,24 +63,54 @@ const Categorias = ({data}) => {
     </Layout>
   );
 };
+Categorias.propTypes = {
+  pageContext: PropTypes.shape({
+    categoria: PropTypes.string.isRequired,
+  }),
+  data: PropTypes.shape({
+    allMarkdownRemark: PropTypes.shape({
+      totalCount: PropTypes.number.isRequired,
+      edges: PropTypes.arrayOf(
+        PropTypes.shape({
+          node: PropTypes.shape({
+            frontmatter: PropTypes.shape({
+              title: PropTypes.string.isRequired,
+            }),
+            fields: PropTypes.shape({
+              slug: PropTypes.string.isRequired,
+            }),
+          }),
+        }).isRequired
+      ),
+    }),
+  }),
+};
 
 export default Categorias;
 
 export const query = graphql`
-  query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      html
-      frontmatter {
-        title
-        categoria
-        featuredimage
-        date(formatString: "MM-DD-YYYY")
-        short_description
-        tags
-      }
-      fields{
-          slug
+  query($categoria: String) {
+    allMarkdownRemark(
+      limit: 2000
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { categoria: { in: [$categoria] } } }
+    ) {
+      totalCount
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "DD MMMM, YYYY")
+            featuredimage
+            tags
+            title
+            categoria
+            short_description
+          }
+          fields {
+            slug
+          }
+        }
       }
     }
   }
-`
+`;
